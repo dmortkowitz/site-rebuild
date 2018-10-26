ab4813ba86dca5d1fe37cc744d1d5263e0b2b478
David Mortkowitz <dmortkowitz@gmail.com>
2018-10-26 08:51:46 -0700

    Add bootstrap installation to application.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ImageCarouselComponent } from './image-carousel/image-carousel.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageCarouselComponent
   ],
   imports: [
     BrowserModule
