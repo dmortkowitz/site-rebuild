f0d959e12972ca80155a53e1ee068b4dcceca028
David Mortkowitz <dmortkowitz@gmail.com>
2018-11-02 11:27:03 -0700

    User authorization returning uncaught error when inspecting page.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,4 +5,12 @@
 
 export const environment = {
   production: false
+  // firebase: {
+  //   apiKey: "AIzaSyDiFj13IRP8ZWqT7o0VVX2HLj5nQZc9yGQ",
+  //   authDomain: "site-rebuild-9136a.firebaseapp.com",
+  //   databaseURL: "https://site-rebuild-9136a.firebaseio.com",
+  //   projectId: "site-rebuild-9136a",
+  //   storageBucket: "site-rebuild-9136a.appspot.com",
+  //   messagingSenderId: "455287499947"
+  // }
 };
